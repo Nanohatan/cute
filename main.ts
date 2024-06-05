5f97823426de1e3125ccc3ffdcd1f360aeb6b24c
Nanohatan <e185730@ie.u-ryukyu.ac.jp>
2024-06-05 19:36:32 +0900

    add image

--- main.ts
+++ main.ts
@@ -4,11 +4,17 @@ const server = pogo.server({ port : 3000 });
 server.router.get('/', (request, h) => {
     return h.file('index.html');
 });
+server.router.get('/character', (request, h) => {
+    return h.file('html/character.html');
+});
 server.router.get('/about', () => {
     return 'about!';
 });
 server.router.get('/contact', () => {
     return 'contact!';
+});
+server.router.get('/assets/{file*}', (request, h) => {
+    return h.directory('assets');
 });
 
 
